a58626802f75b7ad1d80e3d660a817ced7c591d0
Niranjan  sheelavant <niranjansheelavant.ns@gmail.com>
2025-11-21 11:00:06 +0530

    Update pricetax.py

--- pricetax.py
+++ pricetax.py
@@ -1,32 +1,26 @@
 import sys
 
-
 if len(sys.argv) != 3:
-    print("Usage: python pricetax.py <product_price> <tax_percentage>")
-    sys.exit(1)
-
-
-productprice = float(sys.argv[1])
-producttax = float(sys.argv[2])
+    print("No inputs given, using default values...")
+    productprice = 1000
+    producttax = 2
+else:
+    productprice = float(sys.argv[1])
+    producttax = float(sys.argv[2])
+if productprice<=0:
+    print("enter correct input")
+elif producttax<=0:
+    print("tax must be inculde it madidtory")
 
 
 else:
-    productprice = 1000
-    producttax = 2
-
-
-
-
-taxamount = (productprice * producttax) / 100
-totalamount = productprice + taxamount
-
-if productprice <= 0:
-    print("Product price must be greater than 0")
-    sys.exit(1)
-elif producttax < 0:
-    print("Tax percentage must be greater than or equal to 0")
-    sys.exit(1)
+    taxamount = (productprice * producttax) / 100
+    totalamount = productprice + taxamount
     print("Product Price =", productprice)
     print("Product Tax % =", producttax)
     print("Tax Amount =", taxamount)
     print("Total Product Price =", totalamount)
+
+
+
+
