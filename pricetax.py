d536e5800b1207c8657888f1a572aa6bbd577ddd
Niranjan  sheelavant <niranjansheelavant.ns@gmail.com>
2025-11-21 10:50:57 +0530

    Update pricetax.py

--- pricetax.py
+++ pricetax.py
@@ -10,18 +10,22 @@ productprice = float(sys.argv[1])
 producttax = float(sys.argv[2])
 
 
+else:
+    productprice = 1000
+    producttax = 2
+
+
+
+
+taxamount = (productprice * producttax) / 100
+totalamount = productprice + taxamount
+
 if productprice <= 0:
     print("Product price must be greater than 0")
     sys.exit(1)
 elif producttax < 0:
     print("Tax percentage must be greater than or equal to 0")
     sys.exit(1)
-else:
-  
-    taxamount = (productprice * producttax) / 100
-    totalamount = productprice + taxamount
-
-   
     print("Product Price =", productprice)
     print("Product Tax % =", producttax)
     print("Tax Amount =", taxamount)
